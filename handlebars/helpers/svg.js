509146e2bab9030eba9c485c7034ba5eeb19e627
Charles Miller <cmiller@wikia-inc.com>
2016-10-28 13:16:22 -0700

    Inline all SVG instead of including icons we don't use.

--- handlebars/helpers/svg.js
+++ handlebars/helpers/svg.js
@@ -1,6 +1,39 @@
 var Handlebars = require('handlebars')
+    fs         = require('fs'),
+    jsdom      = require("node-jsdom").jsdom;
+    path       = require('path');
 
-module.exports = function wdsIcon(name, options) {
-  var n = "wds-icons-" + options.hash["name"];
- return new Handlebars.SafeString("<svg><use xmlns:xlink=\"http://www.w3.org/1999/xlink\" xlink:href=\"#" + name + "\"></use></svg>");
+function assetName(category, name, variant) {
+    return "wds-" + category + "-" + name + (variant ? "-" + variant : "");
+}
+
+function classAttr(className, variant) {
+  return className
+    ? className + (variant ? " " + className + "-" + variant : "")
+    : className
+}
+
+module.exports = {
+  svg_ref: function(category, name, options) {
+    return new Handlebars.SafeString("<svg class=\"" +
+      classAttr(options.hash["class"], options.hash["variant"]) +
+      "\"><use xmlns:xlink=\"http://www.w3.org/1999/xlink\" xlink:href=\"#" +
+      assetName(category, name, options.hash["variant"]) + "\"></use></svg>");
+  },
+
+  svg_inline: function(category, name, options) {
+    var assetPath = path.resolve(
+      require.resolve('wds-styles'),
+      "../../svg/" + assetName(category, name, options.hash["variant"]) + ".svg");
+    var asset = fs.readFileSync(assetPath);
+    var classes = classAttr(options.hash["class"], options.hash["variant"]);
+
+    var ele = jsdom("").createElement('div')
+    ele.innerHTML = asset
+    if (classes) { ele.firstChild.setAttribute("class", classes); }
+    if (options.hash["width"]) { ele.firstChild.setAttribute("width", options.hash["width"]); }
+    if (options.hash["height"]) { ele.firstChild.setAttribute("height", options.hash["height"]); }
+
+    return new Handlebars.SafeString(ele.innerHTML);
+  }
 }
